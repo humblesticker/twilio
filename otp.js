8e583037fa6dced3e19cf10bfa53ba59276a6471
Brian Choi <humblesticker@gmail.com>
2025-03-01 15:10:01 -0800

    candidate for deployment

--- otp.js
+++ otp.js
@@ -2,10 +2,7 @@
 // handle inline-hook request from Okta
 //
 exports.handler = function(context, event, callback) {
-   // You can log with console.log
-  console.log('context', context);
-  console.log('event', event);
-
+  console.log(context, event);
   const authHeader = event.request.headers.authorization;
   if(authenticated(authHeader, context.API_KEY, context.API_SECRET) === false)
     return callback(null, setUnauthorized());
@@ -13,50 +10,30 @@ exports.handler = function(context, event, callback) {
   // get to and code from event (okta)
   const messageProfile = event.data.messageProfile;
   const channel = messageProfile.deliveryChannel.toLowerCase();
-  console.log('channel', channel);
-  
+  const to = messageProfile.phoneNumber, code = messageProfile.otpCode;
+  console.log(channel, to, code);
+
   const twilioClient = context.getTwilioClient();
-  sendVoice(twilioClient, context.FROM, messageProfile.phoneNumber, messageProfile.otpCode, callback);
+  if(channel === 'sms') { 
+    const request = twilioClient.messages.create({ from: context.MESSAGE_SERVICE_ID, to, body: getMessageBody(code) }); 
+    sendOtp(request, channel, callback);
+  } else if(channel === 'voice') {
+    const oneByOne = String(code).split('').join('. '); // with . and space char, number will be read one by one
+    const request = twilioClient.calls.create({ from: context.VOICE_PHONENUMBER, to, twiml: getVoiceXML(oneByOne)});
+    sendOtp(request, channel, callback);
+  } else return callback(oktaResponse("FAILURE", "", `${channel} is not supported`));
 };
 
-const sendVoice = (twilioClient, from, to, code, callback) => {
-  // one number at a time
-  code = String(code).split('').join('. ');
-  console.log(code);
-
-  twilioClient.calls
-    .create({ from: "+17149420727", to, twiml: createVoiceXML(code)})
-    .then((message) => {
-      console.log('SMS successfully sent');
-      console.log(message.sid);
-      // Make sure to only call `callback` once everything is finished, and to pass
-      // null as the first parameter to signal successful execution.
+// Make sure to only call `callback` once everything is finished
+// pass null as the first parameter to signal successful execution
+const sendOtp = (request, channel, callback) => {
+  request.then((message) => {
+      console.log(`OTP successfully sent via ${channel}: ${message.sid}`);
       return callback(null, oktaResponse("SUCCESSFUL", message.sid, ""));
     })
     .catch((error) => {
       console.error(error);
-      return callback(oktaResponse("FAILURE", "", ""));
-    });
-};
-
-const sendSms = (twilioClient, from, to, code, callback) => {
-  const body = 'otp:' + code;
-
-  // Use `messages.create` to generate a message. Be sure to chain with `then`
-  // and `catch` to properly handle the promise and call `callback` _after_ the
-  // message is sent successfully!
-  twilioClient.messages
-    .create({ from, to, body })
-    .then((message) => {
-      console.log('SMS successfully sent');
-      console.log(message.sid);
-      // Make sure to only call `callback` once everything is finished, and to pass
-      // null as the first parameter to signal successful execution.
-      return callback(null, oktaResponse("SUCCESSFUL", message.sid, ""));
-    })
-    .catch((error) => {
-      console.error(error);
-      return callback(oktaResponse("FAILURE", "", ""));
+      return callback(oktaResponse("FAILURE", "", error));
     });
 };
 
@@ -82,16 +59,16 @@ const oktaResponse = (status, transactionId, transactionMetadata) => {
   return {
       commands: [{
         type: "com.okta.telephony.action",
-        value: [{
-          status,
-          provider: "Twilio",
-          transactionId,
-          transactionMetadata
+        value: [{ status, provider: "Twilio", transactionId, transactionMetadata
         }],
       }]
   };
 };
 
-const createVoiceXML = (code) => {
+const getMessageBody = (code) => {
+  return `DO NOT SHARE YOUR OCCU CODE: ${code}. We NEVER ask you to verbally verify this code. If someone asks for this code it is a scam. Report fraud to 888-354-6228`;
+};
+
+const getVoiceXML = (code) => {
   return `<Response><Pause length="2"/><Say>Your code is ${code}. Once again, your code is ${code}</Say></Response>`;
 };
